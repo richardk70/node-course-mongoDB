6d62136a42749da814bbafbd90a8f88206c8d820
Richard Keightley <richardk70@gmail.com>
2018-08-16 14:00:36 -0700

    =Hash user password before saving

--- playground/hashing.js
+++ playground/hashing.js
@@ -1,6 +1,22 @@
 const {SHA256} = require('crypto-js');
 const jwt = require('jsonwebtoken');
+const bcrypt = require('bcryptjs');
 
+var password = '213abc!';
+
+// bcrypt.genSalt(10, (err, salt) => {
+//     bcrypt.hash(password, salt, (err, hash) => {
+//         console.log(hash);
+//     });
+// });
+
+var hashedPassword = '$2a$10$kXthTfsMtWKudQBJ9xyTzOBxAFivdJB3usnNpboLxPfUX0aTqTo5O';
+
+bcrypt.compare('123abc!', hashedPassword, (err, res) => {
+    console.log(res);
+});
+
+/*
 var data = {
     id: 10
 };
@@ -11,7 +27,7 @@ var decoded = jwt.verify(token, '123abc');
 console.log('decoded: ', decoded);
 
 
-/*
+
 var message = ' I am user number one, baby!';
 
 var hash = SHA256(message);
