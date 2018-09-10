8b01df2868ffa31a241ba1414c3f4ebef53b95d1
Richard Keightley <richardk70@gmail.com>
2018-09-10 14:10:14 -0700

    =testing DELETE token route

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -172,7 +172,7 @@ describe('PATCH /todos/:id', () => {
       .end(done);
   });
 });
-*/
+
 describe('GET /users/me', () => {
   it('should return user if authenticated', (done) => {
     request(app)
@@ -283,6 +283,23 @@ describe('POST /users/login', () => {
         if (err) return done(err);
 
         User.findById(users[1]._id).then( (user) => {
+          expect(user.tokens.length).toBe(0);
+          done();
+        }).catch( (e) => done(e));
+      });
+  });
+});
+*/
+describe('DELETE /users/me/token', () => {
+  it('should remove auth token on log out', (done) => {
+    request(app)
+      .delete('users/me/token')
+      .set('x-auth', users[0].tokens[0].token)
+      .expect(200)
+      .end( (err, res) => {
+        if (err) return done(err);
+
+        User.findById(users[0]._id).then( (user) => {
           expect(user.tokens.length).toBe(0);
           done();
         }).catch( (e) => done(e));
