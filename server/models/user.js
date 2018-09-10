c9f30289ef8c5f5edb8111bfc8b0aad3c199936b
Richard Keightley <richardk70@gmail.com>
2018-09-10 13:28:40 -0700

    =Added DELETE /users/me/token route

--- server/models/user.js
+++ server/models/user.js
@@ -106,6 +106,15 @@ UserSchema.pre('save', function(next) {
   }
 });
 
+UserSchema.methods.removeToken = function(token) {
+  var user = this;
+  return user.update({
+    $pull: {
+      tokens: { token }
+    }
+  });
+};
+
 var User = mongoose.model('User', UserSchema);
 
 module.exports = {User}
